c8e02fb3c7819a8cdc90cc24bbbeba21180d0e13
kshitijshah <shahksh1011@gmail.com>
2018-09-10 18:00:35 -0400

    ADDED A STRING

--- app/src/main/java/com/group5/android/dynamiclayout/MainActivity.java
+++ app/src/main/java/com/group5/android/dynamiclayout/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String str = "hello ";
         /*ConstraintLayout cl = new ConstraintLayout(this);
         cl.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         setContentView(cl);
