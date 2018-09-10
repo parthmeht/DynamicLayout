97ab851f3acac13f1fcfb705dc37fb287d07b328
parthmeht <parthmeht@gmail.com>
2018-09-10 19:19:20 -0400

    Added new activities

---
+++ app/src/main/java/com/group5/android/dynamiclayout/SelectAvatarActivity.java
@@ -0,0 +1,13 @@
+package com.group5.android.dynamiclayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectAvatarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_avatar);
+    }
+}
