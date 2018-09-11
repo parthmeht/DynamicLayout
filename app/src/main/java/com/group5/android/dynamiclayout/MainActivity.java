76991e051baca2b911855e6f50108fc0f692542d
kshitijshah <shahksh1011@gmail.com>
2018-09-10 20:37:46 -0400

    p1

--- app/src/main/java/com/group5/android/dynamiclayout/MainActivity.java
+++ app/src/main/java/com/group5/android/dynamiclayout/MainActivity.java
@@ -2,6 +2,7 @@ package com.group5.android.dynamiclayout;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -13,43 +14,40 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String avatarString;
+    Context context;
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1){
-            if(resultCode == Activity.RESULT_OK){
-                String result=data.getStringExtra("result");
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView avatarImage = findViewById(R.id.imageView);
-        final Intent i1 = new Intent(this, SelectAvatarActivity.class);
+        context = this.getApplicationContext();
 
+        final Intent i1 = new Intent(this, SelectAvatarActivity.class);
+        ImageView avatarImage = findViewById(R.id.imageView);
         avatarImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivityForResult(i1,1);
             }
         });
-        String str = "hello ";
-        Log.d("HEloo",str);
 
-
-
-
-
-        /*ConstraintLayout cl = new ConstraintLayout(this);
-        cl.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        setContentView(cl);
-        TextView helloText = new TextView(this);
-        helloText.setText(R.string.helloWorld);
-        helloText.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        cl.addView(helloText);*/
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1){
+            if(resultCode == Activity.RESULT_OK){
+
+                avatarString = data.getExtras().get("result").toString();
+                ImageView r = findViewById(R.id.imageView);
+//                r.setImageDrawable(getDrawable());
+
+
+                Log.d("result",data.getExtras().get("result").toString());
+            }
+        }
+    }
+
 }
